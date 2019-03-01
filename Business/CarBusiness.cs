775a93680eaaf6daeab5aaccb28d43b454c1a72d
Simona03 <46841129+Simona03@users.noreply.github.com>
2019-03-01 14:45:40 +0200

    Update CarBusiness.cs
    forgot save :(

--- Business/CarBusiness.cs
+++ Business/CarBusiness.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,54 @@ namespace Business
 {
     class CarBusiness
     {
+        private RepairContext repairContext;
+
+        public List<Car> GetAll()
+        {
+            using (repairContext = new RepairContext())
+            {
+                return repairContext.Cars.ToList();
+            }
+        }
+
+        public Car Get(int id)
+        {
+            using (repairContext = new RepairContext())
+            {
+                return repairContext.Cars.Find(id);
+            }
+        }
+
+        public void Add(Car Car)
+        {
+            using (repairContext = new RepairContext())
+            {
+                repairContext.Cars.Add(Car);
+                repairContext.SaveChanges();
+            }
+        }
+
+        public void Update(Car Car)
+        {
+            using (repairContext = new RepairContext())
+            {
+                var item = repairContext.Cars.Find(Car.Id);
+                repairContext.Entry(item).CurrentValues.SetValues(Car);
+                repairContext.SaveChanges();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (repairContext = new RepairContext())
+            {
+                var item = repairContext.Cars.Find(id);
+                if (item != null)
+                {
+                    repairContext.Cars.Remove(item);
+                    repairContext.SaveChanges();
+                }
+            }
+        }
     }
 }
